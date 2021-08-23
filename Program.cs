e50e623009142b0784fa4614bd88da15fd61d8f2
DeniseYuki <49623035+DeniseYuki@users.noreply.github.com>
2021-08-23 20:48:00 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace Desafio00
 {
@@ -12,49 +16,69 @@ namespace Desafio00
             String cidade;
             int continuar;
 
+            var dic = new Dictionary<string, int>();
             id = 0;
             Console.WriteLine("ID: "+ id);
-
+           
             Console.WriteLine("Digite seu nome");
             nome = Console.ReadLine();
 
+            
+
             Console.WriteLine("Digite sua idade: ");
             idade = int.Parse(Console.ReadLine());
-
+            
+            // adicionando ao dicionario nome e id
+            dic.Add($"Nome : { nome }"+" ID :  ", id);
+            
             Console.WriteLine("Digite o nome da sua cidade: ");
             cidade = Console.ReadLine();
-        
-        
+            foreach (var item in dic)
+
+                Console.WriteLine(item.Key + item.Value);
+
             Console.WriteLine("Deseja continuar cadastrando? [0/1]");
             continuar = int.Parse(Console.ReadLine());
 
             while (continuar == 0)
-               // id++;
-            //Console.WriteLine(id);
+               
             if (continuar == 0)
                 {
                     id++;
+                    var cadastro = new Dictionary<string, int>();
+
                     Console.WriteLine("ID: " + id);
                     
                     Console.WriteLine("Digite seu nome");
                     nome = Console.ReadLine();
-
+                    //cadastro.Add($"Nome : { nome }" + " ID :  ", id);
+                    
                     Console.WriteLine("Digite sua idade: ");
                     idade = int.Parse(Console.ReadLine());
 
                     Console.WriteLine("Digite o nome da sua cidade: ");
                     cidade = Console.ReadLine();
-                    //continue;
+                   
                     Console.WriteLine("Deseja continuar cadastrando? [0/1]");
                     continuar = int.Parse(Console.ReadLine());
-                    
-
-                    if (continuar == 1)
+                    cadastro.Add($"Nome : { nome }" + " ID :  ", id);
+                    foreach (var item in cadastro)
+                    {
+                        Console.WriteLine(item.Key +  item.Value );
+                        if (continuar == 1)
                         break;
-                       //Console.Write("Cadastro Finzalizado!!");
+                        //Console.Write(" Cadastro Finzalizado!!");
+                        
+
+                    }
                 }
-            Console.Write("Cadastro Finzalizado!!");
-            
+
+
+            Console.WriteLine("Cadastro Finzalizado!!");
+
+           // foreach (var item in dic)
+            {
+                //Console.WriteLine(item.Key + item.Value);
 
 
 
@@ -67,9 +91,9 @@ namespace Desafio00
 
 
 
+                Console.ReadKey();
+        }
 
-            Console.ReadKey();
         }
     }
 }
- 
